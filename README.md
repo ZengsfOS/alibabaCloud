f1178436d201215beb9d8ba9ca1745b0ceb5db62
zengsf <zsf961006@163.com>
2023-05-13 14:52:07 +0800

    spingboot父子项目搭建完成

---
+++ README.md
@@ -0,0 +1,11 @@
+###   版本说明
+https://github.com/alibaba/spring-cloud-alibaba/wiki/%E7%89%88%E6%9C%AC%E8%AF%B4%E6%98%8E
+
+* Spring Cloud Alibaba Version    ： 2.2.7.RELEASE
+* Spring Boot Version   ：2.3.12.RELEASE
+* Spring Cloud Version  ： Spring Cloud Hoxton.SR12
+* Sentinel Version：   1.8.1
+* Nacos Version   2.0.3
+* RocketMQ Version    4.6.1
+* Dubbo Version   2.7.13
+* Seata Version   1.3.0
---
+++ stock/src/main/java/com/zengsf/stock/controller/StockController.java
@@ -0,0 +1,17 @@
+package com.zengsf.stock.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/stock")
+public class StockController {
+
+    @GetMapping("/reduct")
+    public String reduct(){
+        System.out.println("扣减库存");
+        return "扣减库存";
+    }
+}
